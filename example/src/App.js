0e772a997cf6025e31397e96a373cc57aff1dd67
Hoang Nguyen 7 <hoang.nguyen7@sotatek.com>
2023-03-24 15:18:44 +0700

    remove example

--- example/src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from 'react'
-import { SelectWalet } from 'react-wallet-selector'
-
-function App() {
-  return <div className='App'><SelectWalet/></div>
-}
-
-export default App
