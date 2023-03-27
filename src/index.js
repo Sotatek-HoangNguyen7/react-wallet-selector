4f17ae7a605a69a2e74173f06a8f4dea395bf9cb
Hoang Nguyen 7 <hoang.nguyen7@sotatek.com>
2023-03-27 16:58:42 +0700

    add provider

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import React from 'react'
 import Connect from './components/connect'
 import Send from './components/send'
 import Sign from './components/sign'
-import MinaProvider from './provider'
+import AppProvider from './provider'
 
 export const SelectWalet = ({ children }) => {
-  return <Connect />
+  return (
+    <AppProvider>
+      <Connect />
+    </AppProvider>
+  )
 }
 
 export const SendWalet = ({ children }) => {
-  return <Send />
+  return (
+    <AppProvider>
+      <Send />
+    </AppProvider>
+  )
 }
 
 export const SignWalet = ({ children }) => {
-  return <Sign />
-}
-
-export const MinaWalletProvider = ({ children }) => {
-  return <MinaProvider>{children}</MinaProvider>
+  return (
+    <AppProvider>
+      <Sign />
+    </AppProvider>
+  )
 }
--- src/provider/index.js
+++ src/provider/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from '../store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
-const MinaProvider = ({ children }) => {
-  return <div>{children}</div>
+const persistor = persistStore(store)
+const AppProvider = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }
 
-export default MinaProvider
+export default AppProvider
